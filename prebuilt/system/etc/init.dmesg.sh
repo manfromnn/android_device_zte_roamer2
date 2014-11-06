ea28627dec3661ea627d9598485d32c0933bdd01
Andrej <manfromnn@gmail.com>
2014-11-07 00:12:41 +0300

    Init dmesg service

---
+++ prebuilt/system/etc/init.dmesg.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+
+# No path is set up at this point so we have to do it here.
+PATH=/sbin:/system/sbin:/system/bin:/system/xbin
+export PATH
+echo "0" > /proc/sys/kernel/dmesg_restrict
